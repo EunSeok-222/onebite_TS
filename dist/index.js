e25452ad80ce4c735ff3f61893d8cdca3b79cd06
EunSeok-222 <dldmstjr114@naver.com>
2026-03-17 15:41:55 +0900

    first commit

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+console.log("Hello World2");
+const a = 1;
+export {};
---
+++ src/chapter1.ts
@@ -0,0 +1,31 @@
+// 숫자
+let num1: number = 123;
+
+// 문자열
+let str1: string = "hello";
+
+// 불리언
+let bool1: boolean = true;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// 리터럴 -> 특정 값만 허용
+let literal1: "hello" = "hello";
+let literal2: 123 = 123;
+
+// 배열
+let arr1: number[] = [1, 2, 3];
+let arr2: string[] = ["hello", "world"];
+
+// 객체
+let obj1: { name: string; age: number } = { name: "john", age: 30 };
+
+// 함수
+let func1: (name: string) => string = (name) => {
+  return `hello ${name}`;
+};
+
